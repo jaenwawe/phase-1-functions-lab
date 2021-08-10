9b15484dab860449a2b9d695ed4a5d3fc82179a9
jaenwawe <nwawe.jae@gmail.com>
2021-08-10 11:49:33 -0500

    commit tree 9a75900b5de783985d25fdb93b0efb4b72177164

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-// Code your solution in this file!
+const walk = 264;
+let beginning;
+let distance;
+let destinatin,destination ;
+let start;
+
+function distanceFromHqInBlocks (destinatin){
+    let start = 42;
+    if (destinatin > start)
+    return destinatin - start;
+
+    return start - destinatin;
+}
+
+ function  distanceFromHqInFeet(destinatin){
+
+    return distanceFromHqInBlocks (destinatin) * walk;
+ }
+
+ function  calculatesFarePrice(start,destination){
+    let tripFeet = distanceTravelledInFeet (start, destination);
+    tripFeet = tripFeet - 400;
+    let fare = 0;
+
+    if (tripFeet <= 1600 && tripFeet > 0)
+    {
+        fare = tripFeet *.02;
+        return fare;
+    }
+
+     else if (1600 < tripFeet &&  tripFeet <= 2100){
+        fare = 25;
+        return fare;
+    }
+    else  if (tripFeet >= 2100){
+        
+        return 'cannot travel that far';
+    }
+    return fare;
+}
+
+function distanceTravelledInFeet (start, destinatin){
+
+    if (destinatin > start){
+    return Math.abs((destinatin - start) * walk);
+
+    }
+
+    return (start - destinatin) * walk;
+}
+
